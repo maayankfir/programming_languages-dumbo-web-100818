4507ebd23be84e6116c0a31821594acb6170877b
Maayan Kfir <mayankfir@gmail.com>
2018-09-28 18:18:32 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,6 +4,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language_name|
   language_name.each do |name, type|
+    if new_hash[name] == nil
     new_hash[name] = type
     new_hash[name][:style] = []
     new_hash[name][:style] << style
